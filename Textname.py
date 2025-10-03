2ed9711afe9ab5b0b318b429f32bb99a540333b2
priyanka5123 <priyanka.k8100@gmail.com>
2025-10-03 14:25:36 -0700

    added basicform

---
+++ Textname.py
@@ -0,0 +1,32 @@
+from flask import Flask, redirect, url_for, request
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    # Inline HTML form
+    return '''
+        <html>
+           <body>
+              <form action="/login" method="post">
+                 <p>Enter Name:</p>
+                 <p><input type="text" name="nm" /></p>
+                 <p><input type="submit" value="Submit" /></p>
+              </form>
+           </body>
+        </html>
+    '''
+@app.route('/success/<name>')
+def success(name):
+    return f'Welcome {name}'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+if __name__ == '__main__':
+    app.run(debug=True)
