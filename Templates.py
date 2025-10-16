b5bbb94a644f717c3368a4427136b6bfd27664d3
priyanka5123 <priyanka.k8100@gmail.com>
2025-10-15 17:30:51 -0700

    added score

--- Templates.py
+++ Templates.py
@@ -2,8 +2,13 @@ from flask import Flask, render_template
 app =Flask(__name__)
 
 @app.route('/hello/<user>')
-def hello_name(user):
+def hello_name(user): # http://127.0.0.1:5000/hello/max
     return render_template('hello.html',name= user)
+@app.route('/score')  #http://127.0.0.1:5000/score
+def result():
+    dict = {'phy':50,'che':60,'maths':70}
+
+    return render_template('score.html', result = dict)
 
 if __name__ == '__main__':
     app.run(debug = True)
